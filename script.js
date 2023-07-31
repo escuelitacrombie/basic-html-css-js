e56b0a2ab0407a746c59cd46622ce77c2a3719bb
Ornella <nelitagrigolato@gmail.com>
2023-07-31 18:37:43 -0300

    Ejercitacion de Frontend

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function checkGuess() {
     console.log("randomNumber", randomNumber);
     attempts++
     let guess = parseInt(document.getElementById('guessInput').value);
-    let result = document.getElementById('result');
-    let attemptsDOM = document.getElementById('attempts');
+    const result = document.getElementById('result');
+    const attemptsDOM = document.getElementById('attempts');
 
     if (guess === randomNumber) {
         result.innerHTML = '¡Felicitaciones! ¡Adivinaste el número!';
-    } else if(guess === -1 || guess === 100){
+    } else if(guess < 1 || guess >= 100){
         result.innerHTML = 'El numero ingresado es invalido, ingesa un numero entre 1 y 100';
     } else if (guess < randomNumber) {
         result.innerHTML = 'Intenta con un número más alto.';
