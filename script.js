73b12cb10f9ee75ada969c5da440ada143a67810
Giovistica <giovi.rhcp@gmail.com>
2023-07-31 20:14:26 -0300

    hice la tarea

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let int = 0
 
 function checkGuess() {
     console.log("randomNumber", randomNumber);
     let guess = parseInt(document.getElementById('guessInput').value);
     let result = document.getElementById('result');
+    
 
     if (guess === randomNumber) {
         result.innerHTML = '¡Felicitaciones! ¡Adivinaste el número!';
-    } else if (guess < randomNumber) {
+        int=0;
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+    }else if(guess < 0){
+        result.innerHTML = 'El valor ingresado no es correcto, debes ingresar un numero mayor que 0.';
+    }else if(guess > 100){
+        result.innerHTML = 'El valor ingresado no es correcto, debes ingresar un numero menor que 100.';
+    }  else if (guess < randomNumber) {
+        int ++
         result.innerHTML = 'Intenta con un número más alto.';
     } else {
         result.innerHTML = 'Intenta con un número más bajo.';
+        int ++
     }
+    intentos.innerHTML =  'Llevas '+ int +' intentos.';
+    
 }
