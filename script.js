a5074eed5947883430c1ccdc03b75fc18a3a728b
Melany Kunzi <mela74020@gmail.com>
2023-08-02 17:22:31 -0300

    cambios

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-let numIntentos = 0
-
+let randomNumber;
+let numIntentos = 0;
+document.addEventListener("DOMContentLoaded", function() {
+    resetGame()
+});
+function resetGame(){
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    numIntentos = 0
+    intentos.innerHTML = 'Intentos: ' + numIntentos
+    result.innerHTML = ''
+    document.getElementById('guessInput').value = '';
+}
 function checkGuess() {
         console.log("randomNumber", randomNumber);
         let guess = parseInt(document.getElementById('guessInput').value);
@@ -13,7 +22,7 @@ function checkGuess() {
         } else if (guess > randomNumber && guess>0 && guess<101){
             result.innerHTML = 'Intenta con un número más bajo.';
         } else if(guess < 0 | guess > 101){
-            result.innerHTML = 'Ingresa un numero entre 0 y 100'
+            result.innerHTML = 'Ingresa un numero entre 1 y 100'
         }
         numIntentos++
         intentos.innerHTML = 'Intentos: ' + numIntentos
